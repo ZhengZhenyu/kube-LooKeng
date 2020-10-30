dd5cbfa127a64eb57eaa4c26d4cbfbd79f69756b
Zhenyu Zheng <zheng.zhenyu@outlook.com>
2020-10-30 09:56:39 +0800

    Create docker-entrypoint.sh

---
+++ dockerfile/airpal/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cd $AIRPAL_HOME
+
+if [ "$1" = 'airpal' ]; then
+	: ${AIRPAL_HOME:="/airpal"}
+	: ${USER_TIMEZONE:="UTC"}
+
+	: ${MYSQL_HOST:="mysql"}
+	: ${MYSQL_PORT:="3306"}
+	: ${MYSQL_DB:="airpal"}
+	: ${MYSQL_USER:="airpal"}
+	: ${MYSQL_PASSWD:="airpal"}
+	: ${PRESTO_SERVER_URI:="http://presto:8080"}
+	
+	sed -ri 's/(user:).*/\1 "'"$MYSQL_USER"'"/' "$AIRPAL_HOME/reference.yml"
+	sed -ri 's/(password:).*/\1 "'"$MYSQL_PASSWD"'"/' "$AIRPAL_HOME/reference.yml"
+	sed -ri 's|(url:).*|\1 "'"jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DB"'"|' "$AIRPAL_HOME/reference.yml"
+	sed -ri 's|(prestoCoordinator:).*|\1 "'"$PRESTO_SERVER_URI"'"|' "$AIRPAL_HOME/reference.yml"
+fi
+
+exec "$@"
