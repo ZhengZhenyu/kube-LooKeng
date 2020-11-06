c4166cbee2bc90dd5cdccd52055efb30a18fc0f8
Zhenyu Zheng <zheng.zhenyu@outlook.com>
2020-11-06 14:50:10 +0800

    Update entrypoint.sh

--- dockerfile/openLooKeng/entrypoint.sh
+++ dockerfile/openLooKeng/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 IS_COORDINATOR=${IS_COORDINATOR:-""}
-if [ "$IS_COORDINATOR" != "true" ]; then
+if [ "$IS_COORDINATOR" == "true" ]; then
     echo "coordinator=true" >> etc/config.properties
 else
     echo "coordinator=false" >> etc/config.properties
